90e05415fd44f1282e8e003e45590fed52c1227f
lrmedeiros <50423902+lrmedeiros@users.noreply.github.com>
2019-05-08 16:36:04 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <locale.h>
+#define TAM 2
+
+int main()
+{
+    /* Adicionando caracteres do português /*/
+    setlocale(LC_ALL,"Portuguese");
+
+    /* Declaração de variáveis /*/
+    char nomes[TAM][100], auxiliarNome[100];
+    float alturas[TAM],auxiliar,media,acmAlt,somador;
+    int i, j,desvio,opcao;
+
+    /* Captação dos dados /*/
+    for(i = 0 ; i < TAM ; i++){
+        printf("Digite o nome do %iº jogador: ",(i+1));
+        gets(nomes[i]);
+        system("cls");
+    }
+    for(i = 0 ; i < TAM ; i++){
+        do{
+            printf("Digite a altura de %s: ",nomes[i]);
+            scanf("%f",&alturas[i]);
+            acmAlt+=alturas[i];
+            somador+=pow(alturas[i],2);
+            if(alturas[i] <= 0){
+                printf("ALTURA INVÁVILDA!!!\n");
+                system("pause");
+            }
+            system("cls");
+        }while(alturas[i] <= 0);
+    }
+    for(i = 0; i<TAM;i++){
+        for(j=i;j<TAM;j++){
+            if(alturas[j]<alturas[i]){
+                auxiliar = alturas[i];
+                alturas[i] = alturas[j];
+                alturas[j] = auxiliar;
+                strcpy(auxiliarNome,nomes[i]);
+                strcpy(nomes[i],nomes[j]);
+                strcpy(nomes[j],auxiliarNome);
+            }
+        }
+    }
+
+    /* Criação do menu /*/
+    do{
+    printf("======== CLEVELAND CAVALIERS ========\n");
+    printf("1 – Dados dos jogadores\n");
+    printf("2 – Média de alturas\n");
+    printf("3 – Desvio padrão das alturas\n");
+    printf("4 – Maior e Menor altura\n");
+    printf("5 – Mediana das alturas\n");
+    printf("6 – Finaliza\n");
+    printf("Digite uma opção: ");
+    scanf("%i",&opcao);
+    switch(opcao)
+    {
+        case 1: /* item 1/*/
+            for(i = 0 ; i < TAM ; i++){
+                printf("\nJOGADOR: %s\n",nomes[i]);
+                printf("ALTURA: %.2fm\n",alturas[i]);
+            }
+            system("pause");
+            system("cls");
+            break;
+        case 2: /* item 2/*/
+            media=acmAlt/TAM;
+            printf("A média dos jogadores é igual a %.2f metros\n", media);
+            system("pause");
+            system("cls");
+            break;
+        case 3: /* item 3/*/
+            if(media==0 || media<0){
+                printf("Não é possível calcular o desvio padrão sem a média\n");
+                system("pause");
+                system("cls");
+            }else{
+                desvio=(somador/TAM)-pow(media,2);
+                printf("O desvio padrão é igual a %.2f\n", desvio);
+                system("pause");
+                system("cls");
+            }
+            break;
+
+    }
+    }while(opcao != 6);
+
+
+    return 0;
+}
